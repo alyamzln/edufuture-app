846bbf262a33187bc64d8cfa1d1b47fc99f00132
Irdina <irdinairwan1@gmail.com>
2022-12-27 23:37:26 +0800

    Video Meeting Added

--- app/src/main/java/com/example/quizsection/StudyRoom.java
+++ app/src/main/java/com/example/quizsection/StudyRoom.java
@@ -1,14 +1,73 @@
 package com.example.quizsection;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class StudyRoom extends AppCompatActivity {
+
+    EditText codeBox;
+    Button joinBtn, shareBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_room);
+
+        codeBox = findViewById(R.id.codeBox);
+        joinBtn = findViewById(R.id.joinBtn);
+        shareBtn = findViewById(R.id.shareBtn);
+
+        URL serverURL = null;
+        try {
+            serverURL = new URL("https://meet.jit.si");
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        JitsiMeetConferenceOptions defaultOptions
+                = new JitsiMeetConferenceOptions.Builder()
+                .setServerURL(serverURL)
+                .setFeatureFlag("welcomepage.enabled", false)
+                .build();
+
+        JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+
+        joinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                JitsiMeetConferenceOptions options
+                        = new JitsiMeetConferenceOptions.Builder()
+                        .setRoom(codeBox.getText().toString()).setFeatureFlag("welcomepage.enabled", false)
+                        .build();
+                JitsiMeetActivity.launch(StudyRoom.this, options);
+            }});
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String string = codeBox.getText().toString();
+                Intent intent = new Intent();
+                intent.setAction(intent.ACTION_SEND);
+                intent.putExtra(intent.EXTRA_TEXT, string);
+                intent.setType("text/plain");
+                startActivity(intent);
+            }});
+
     }
+
 }
