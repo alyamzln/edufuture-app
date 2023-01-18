99418a48b6ec7d119998a5820fb316f0e3cae98f
frhanaalmshh <farhanalamshah@gmail.com>
2023-01-18 20:15:20 +0800

    Add Content View

---
+++ app/src/main/java/com/example/quizsection/AdminCoursePage.java
@@ -0,0 +1,13 @@
+package com.example.quizsection;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminCoursePage extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_courses_page);
+    }
+}
