6f1babfee8f424374e227260d85e973aeadb7f02
alyamzln <nur27alya@gmail.com>
2023-01-29 23:19:59 +0800

    Update settings and room bg

--- app/src/main/java/com/example/quizsection/QuizReady.java
+++ app/src/main/java/com/example/quizsection/QuizReady.java
@@ -8,16 +8,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class QuizReady extends AppCompatActivity {
 
     private Button startQuiz;
     private Dialog loadingDialog;
+    private ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_ready);
+
+        back = findViewById(R.id.back_btn_quiz3);
 
         loadingDialog = new Dialog(QuizReady.this);
         loadingDialog.setContentView(R.layout.loading_progressbar);
@@ -27,6 +31,15 @@ public class QuizReady extends AppCompatActivity {
 
         startQuiz = findViewById(R.id.startQuiz);
 
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(QuizReady.this, QuizChapters.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
         startQuiz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,9 +47,9 @@ public class QuizReady extends AppCompatActivity {
                 loadingDialog.show();
                 Intent intent = new Intent(QuizReady.this, QuizQuestions.class);
                 startActivity(intent);
+                loadingDialog.dismiss();
             }
         });
-
     }
 
 }
